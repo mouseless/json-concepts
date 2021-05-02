6202a28bc17258af630e21041610b1f58c7354bb
Cihan Deniz <cih.deniz@gmail.com>
2021-05-02 20:56:06 +0300

    create empty tests of explicit concepts

---
+++ impl/nodejs/test/spec/07-concepts/03-explicit-concepts.js
@@ -0,0 +1,9 @@
+describe('spec/concepts/explicit-concepts', function () {
+    it('should set concept explicitly');
+    it('should give error when concept does not exist');
+    it('should give syntax error when nothing follows type symbol');
+
+    describe('resolving literal conflicts', function () {
+        it('should set concept explicitly');
+    });
+});
